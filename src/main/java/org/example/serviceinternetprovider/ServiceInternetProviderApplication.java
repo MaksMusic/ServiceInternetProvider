37b09722105556702cb1139997b387820d6075ad
MaksMusic <MaksMusic05@yandex.ru>
2026-08-14 20:54:50 +0300

    Initial commit

---
+++ src/main/java/org/example/serviceinternetprovider/ServiceInternetProviderApplication.java
@@ -0,0 +1,13 @@
+package org.example.serviceinternetprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInternetProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceInternetProviderApplication.class, args);
+    }
+
+}
