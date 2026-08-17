d9ab261ee07db97785a44d3381327a216ea5c703
dimaz <dima.z.u@mail.ru>
2026-08-17 21:19:04 +0300

    выполнил все 3 задания в 1 т.к. не работал гит тут

--- src/main/java/org/example/serviceinternetprovider/Controller/InfoController.java
+++ src/main/java/org/example/serviceinternetprovider/Controller/InfoController.java
@@ -1,6 +1,5 @@
-package org.example.serviceinternetprovider;
+package org.example.serviceinternetprovider.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
